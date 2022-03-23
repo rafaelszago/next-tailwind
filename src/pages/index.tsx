47dc0aa502f1b52a918934f5b17b276533982ba1
Rafael Zago <rafaelszago@gmail.com>
2022-03-23 16:42:24 -0300

    feat: add sample page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,53 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Next + Tailwind</title>
-        <meta name="description" content="Next + tailwind starter project" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
-  );
-};
 
-export default Home;
+      <main>
+        <div className="bg-slate-900 min-h-screen flex items-center">
+          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            <div className="text-center">
+              <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+                Starter project
+              </h2>
+              <p className="mt-1 text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
+                Next + Tailwind
+              </p>
+              <p className="max-w-xl mt-5 mx-auto text-xl text-gray-400">
+                Starter a project easily with Next + Tailwind
+              </p>
+              <div>
+                <a
+                  href="#"
+                  className="w-full inline-flex space-x-4 max-w-fit mt-6 justify-center py-2 px-4 border border-slate-400 rounded-md shadow-sm bg-transparent text-sm font-medium text-slate-500 hover:bg-slate-800 hover:text-white hover:border-white transition-all duration-300"
+                >
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>GitHub project</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
