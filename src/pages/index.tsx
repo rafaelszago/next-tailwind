322fbbe255b7b1d20c11e75eec1a28efe5cfa993
Rafael Zago <rafaelszago@gmail.com>
2022-03-23 15:46:10 -0300

    chore: remove inital files

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next + Tailwind</title>
+        <meta name="description" content="Next + tailwind starter project" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
